1b285c9ff2878e702f6e7b20efb1a12351777f69
Aaronwzs <88762614+Aaronwzs@users.noreply.github.com>
2024-08-25 00:31:04 +0800

    index changes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,10 +5,12 @@
       
 </head>
   <body data-home-page="Page-1.html" data-home-page-title="Page 1" data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="en">
-    <header class="u-clearfix u-header u-header" id="sec-9cc3">
-        @include('home.header')
 
-        </header>
+       
+
+        
+
+        @include('home.header')
 
         @include('home.mainscreen')
     
@@ -17,52 +19,7 @@
         @include ('home.promotion')
 
         @include ('home.addition_details')
-    <section class="u-clearfix u-image u-section-7" id="carousel_0c50" data-image-width="1706" data-image-height="1080" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-        <div class="u-container-align-center u-container-style u-expanded-width-xs u-group u-shape-rectangle u-group-1">
-          <div class="u-container-layout u-valign-bottom u-container-layout-1"><span class="u-align-center u-file-icon u-icon u-text-white u-icon-1"><img src="images/4338295-97b12486.png" alt=""></span>
-            <p class="u-align-center u-large-text u-text u-text-body-alt-color u-text-variant u-text-1"> These voyages offer a proper break from the every-day, an opportunity to ta​ke an inner journey, one where peace of mind and calm hold sway. We will not seek to empty but to fill ourselves with the energy elements around us, and manifestations of the body, mind, emotions, and sensations.&nbsp;<br>
-            </p>
-            <p class="u-align-center u-text u-text-body-alt-color u-text-2">Image from&nbsp;<a href="https://www.freepik.com/" class="u-border-active-black u-border-hover-black u-border-none u-btn u-button-link u-button-style u-none u-text-body-alt-color u-btn-1" target="_blank" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">Freepik</a>
-            </p>
-            <a href="#" class="u-active-palette-2-base u-align-left u-border-active-palette-2-base u-border-hover-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-palette-3-base u-radius-50 u-text-active-white u-text-body-color u-text-hover-white u-btn-2" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">read more</a>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="u-align-center u-clearfix u-image u-section-9" id="carousel_7d76" data-image-width="1706" data-image-height="1080">
-      <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-xl u-sheet-1">
-        <h2 class="u-align-left u-custom-font u-font-montserrat u-text u-text-body-alt-color u-text-1"> Benefits of sport training</h2>
-        <a href="#" class="u-active-palette-2-base u-align-left u-border-active-palette-2-base u-border-hover-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-palette-3-base u-radius-50 u-text-active-white u-text-body-color u-text-hover-white u-btn-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">read more</a>
-      </div>
-    </section>
-    <section class="u-clearfix u-image u-section-10" id="carousel_c825" data-image-width="1500" data-image-height="1001">
-      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
-        <div class="u-align-left u-form u-form-1">
-          <form action="https://forms.nicepagesrv.com/v2/form/process" class="u-clearfix u-form-spacing-25 u-form-vertical u-inner-form" source="email" name="form" style="padding: 0px;">
-            <h2 class="u-align-left u-custom-font u-font-montserrat u-form-group u-form-text u-text u-text-body-alt-color u-text-1"> Join Us</h2>
-            <p class="u-align-left u-form-group u-form-text u-text u-text-body-alt-color u-text-2"> Any questions or remarks? Just write us a message! </p>
-            <div class="u-form-email u-form-group u-label-none">
-              <label for="email-bb9b" class="u-label">Email</label>
-              <input type="email" placeholder="Enter a valid email address" id="email-bb9b" name="email" class="u-custom-font u-font-montserrat u-input u-input-rectangle u-radius-50 u-input-1" required="">
-            </div>
-            <div class="u-align-left u-form-group u-form-submit u-label-none">
-              <input type="submit" value="submit" class="u-form-control-hidden">
-              <a href="#" class="u-active-palette-2-light-2 u-border-none u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-2-light-2 u-palette-2-base u-radius-50 u-text-active-palette-2-base u-text-hover-palette-2-base u-text-white u-btn-1">Submit</a>
-            </div>
-            <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
-            <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
-            <input type="hidden" value="" name="recaptchaResponse">
-            <input type="hidden" name="formServices" value="7029c52b-d9c0-0e2f-51d5-4ef4578b7c79">
-          </form>
-        </div>
-        <p class="u-text u-text-body-alt-color u-text-default u-text-3">Image from <a href="https://freepik.com" class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-white u-btn-2">Freepik</a>
-        </p>
-      </div>
-    </section>
-    
-    
-    
-    @include ('home.footer')
+  
+        @include ('home.footer')
   
 </body></html>
